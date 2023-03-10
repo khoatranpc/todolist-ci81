803b42a0b99a3a4bdeff66fa844e8d335577fde4
khoatranpc <khoatranpc603@gmail.com>
2023-03-10 21:13:41 +0700

    fix bug

---
+++ src/dictionary.js
@@ -0,0 +1,13 @@
+export const LANG = {
+    VI: {
+        'A_1': 'Tên công việc',
+        'A_2': 'Chi tiết',
+        'A_3': 'Trạng thái'
+    },
+    EN: {
+        'A_1': 'Name',
+        'A_2': 'Description',
+        'A_3': 'Status'
+    }
+}
+//  LANG['EN']['A_1']
